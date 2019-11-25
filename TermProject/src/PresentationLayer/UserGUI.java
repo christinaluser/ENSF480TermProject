1f1cfcf28166daa9275b6d3793c028751a5ced59
Christina <christinalu42@gmail.com>
2019-11-25 11:18:02 -0700

    .

--- TermProject/src/PresentationLayer/UserGUI.java
+++ TermProject/src/PresentationLayer/UserGUI.java
@@ -1,47 +1,56 @@
 package PresentationLayer;
 
-import Controller.RenterListener;
+import Controller.LoginListener;
+import Controller.UserListener;
+import com.mysql.cj.xdevapi.Table;
 
 import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class RenterGUI extends UserGUI {
+public class UserGUI extends TableGUI {
     private JFrame frame;
-    private JPanel panel;
     private JButton searchButton;
-    private JButton notificationsButton;
+    private JPanel panel;
     private JComboBox type;
-    private JSpinner noBed;
-    private JSpinner noBath;
     private JComboBox isFurnished;
     private JComboBox cityQuadrant;
     private JComboBox priceRange;
+    private JSpinner noBed;
+    private JSpinner noBath;
     private JButton showAllButton;
-    private JButton logoutButton;
-    private RenterListener listener;
+    private JButton loginButton;
+    private UserListener listener;
     private String[] headers;
 
-    public RenterGUI() {
+    public UserGUI() {
         headers = new String[]{"ID", "Type", "Rent", "Property #", "Street", "Postal Code", "City Quadrant", "Bedrooms",
                 "Bathrooms", "Furnished", "Contact Landlord"};
     }
 
-    public RenterGUI(RenterListener l) {
+    public UserGUI(UserListener l) {
         headers = new String[]{"ID", "Type", "Rent", "Property #", "Street", "Postal Code", "City Quadrant", "Bedrooms",
                 "Bathrooms", "Furnished", "Contact Landlord"};
         listener = l;
-        $$$setupUI$$$();
+        searchProperties();
+        showAllProperties();
     }
 
-    private void showAllProperties() throws IOException {
-        String result = listener.actionPerformed("DISPLAY");
-//        test
-//        result = "1/apt/$400/44/street1/g3h 4t3/ne/3/2/furnished;2/apt/200/44/street1/g3h 4t3/se/4/3/unfurnished";
-        if (result.equals(null)) {
-            JOptionPane.showMessageDialog(new JFrame(), "No properties posted at the moment!");
+    public String getCriteria() {
+        return type.getSelectedItem() + "/" + noBed.getValue() + "/" + noBath.getValue()
+                + "/" + getIsFurnished() + "/" + cityQuadrant.getSelectedItem() + "/" + priceRange.getSelectedItem();
+    }
+
+    public boolean getIsFurnished() {
+        if (isFurnished.getSelectedIndex() == 0) {
+            return true;
         } else {
-            showTable(headers, result, panel);
+            return false;
         }
     }
 
@@ -49,6 +58,15 @@ public class RenterGUI extends UserGUI {
         String result = listener.actionPerformed("SEARCH" + "/" + getCriteria());
 //        test
 //        result = "1/house/$100/44/street1/g3h 4t3/ne/3/2/furnished;2/apt/200/44/street1/g3h 4t3/se/4/3/unfurnished";
+        if (result.equals("null")) {
+            JOptionPane.showMessageDialog(new JFrame(), "No properties found!");
+        } else {
+            showTable(headers, result, panel);
+        }
+    }
+
+    private void showAllProperties() throws IOException {
+        String result = listener.actionPerformed("DISPLAY");
         if (result.equals(null)) {
             JOptionPane.showMessageDialog(new JFrame(), "No properties found!");
         } else {
@@ -56,21 +74,10 @@ public class RenterGUI extends UserGUI {
         }
     }
 
-    private void showNotifiedProperties() throws IOException {
-        String result = listener.actionPerformed("DISPLAYNOTIFIED");
-//        test
-//        result = "2/apt/200/44/street1/g3h 4t3/se/4/3/unfurnished";
-        if (result.equals(null)) {
-            JOptionPane.showMessageDialog(new JFrame(), "No properties match your criteria at the moment");
-        } else {
-            showTable(headers, result, panel);
-        }
-    }
-
     public void activateButtons() {
-        notificationsButton.addActionListener(e -> {
+        showAllButton.addActionListener(e -> {
             try {
-                showNotifiedProperties();
+                showAllProperties();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -82,36 +89,24 @@ public class RenterGUI extends UserGUI {
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-
         });
 
-        showAllButton.addActionListener(e -> {
-            try {
-                showAllProperties();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        });
+        loginButton.addActionListener(e -> listener.changeGUI(new LoginGUI(new LoginListener(listener.getClient()))));
     }
 
     @Override
     public void tableButtonClicked(String propertyId, String colName) {
-        EmailLandlord dialog = new EmailLandlord();
-        dialog.setTitle(colName + " Property ID: " + propertyId);
+        EmailLandlordUnregistered dialog = new EmailLandlordUnregistered();
         dialog.pack();
         dialog.setVisible(true);
-
-        try {
-            listener.actionPerformed("EDIT/" + propertyId + "/" + dialog.emailBody.getText());
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
     @Override
     public void updateView() {
+        ((SpinnerNumberModel) noBed.getModel()).setMinimum(0);
+        ((SpinnerNumberModel) noBath.getModel()).setMinimum(0);
         frame = new JFrame("ProperTee");
-        frame.setContentPane(panel);
+        frame.setContentPane(this.panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -119,13 +114,20 @@ public class RenterGUI extends UserGUI {
     }
 
 //    public static void main(String[] args) {
-//        RenterGUI gui = new RenterGUI();
+//        UserGUI gui = new UserGUI();
 //        gui.updateView();
 //    }
 
-    @Override
+
     public void close() {
         frame.dispose();
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
     }
 
     /**
@@ -136,52 +138,11 @@ public class RenterGUI extends UserGUI {
      * @noinspection ALL
      */
     private void $$$setupUI$$$() {
-        createUIComponents();
-<<<<<<< HEAD
-        panel = new javax.swing.JPanel();
-        panel.setLayout(new java.awt.BorderLayout(0, 0));
-        final javax.swing.JToolBar toolBar1 = new javax.swing.JToolBar();
-        panel.add(toolBar1, BorderLayout.NORTH);
-        final javax.swing.JLabel label1 = new javax.swing.JLabel();
-        toolBar1.add(label1);
-        type = new javax.swing.JComboBox();
-        toolBar1.add(type);
-        final javax.swing.JLabel label2 = new javax.swing.JLabel();
-        toolBar1.add(label2);
-        toolBar1.add(noBed);
-        final javax.swing.JLabel label3 = new javax.swing.JLabel();
-        toolBar1.add(label3);
-        toolBar1.add(noBath);
-        isFurnished = new javax.swing.JComboBox();
-        toolBar1.add(isFurnished);
-        final javax.swing.JLabel label4 = new javax.swing.JLabel();
-        toolBar1.add(label4);
-        cityQuadrant = new javax.swing.JComboBox();
-        toolBar1.add(cityQuadrant);
-        final javax.swing.JLabel label5 = new javax.swing.JLabel();
-        toolBar1.add(label5);
-        priceRange = new javax.swing.JComboBox();
-        toolBar1.add(priceRange);
-        searchButton = new javax.swing.JButton();
-        toolBar1.add(searchButton);
-        final javax.swing.JToolBar toolBar2 = new javax.swing.JToolBar();
-        panel.add(toolBar2, BorderLayout.SOUTH);
-        showAllButton = new javax.swing.JButton();
-        toolBar2.add(showAllButton);
-        notificationsButton = new javax.swing.JButton();
-        toolBar2.add(notificationsButton);
-        logoutButton = new javax.swing.JButton();
-        toolBar2.add(logoutButton);
-=======
         panel = new JPanel();
         panel.setLayout(new BorderLayout(0, 0));
-        panel.setPreferredSize(new Dimension(1100, 900));
         final JToolBar toolBar1 = new JToolBar();
         toolBar1.setFloatable(false);
         panel.add(toolBar1, BorderLayout.NORTH);
-        final JLabel label1 = new JLabel();
-        label1.setText("Type: ");
-        toolBar1.add(label1);
         type = new JComboBox();
         final DefaultComboBoxModel defaultComboBoxModel1 = new DefaultComboBoxModel();
         defaultComboBoxModel1.addElement("House");
@@ -190,13 +151,18 @@ public class RenterGUI extends UserGUI {
         defaultComboBoxModel1.addElement("Condo");
         type.setModel(defaultComboBoxModel1);
         toolBar1.add(type);
-        final JLabel label2 = new JLabel();
-        label2.setText("# Bedrooms: ");
-        toolBar1.add(label2);
+        final JLabel label1 = new JLabel();
+        label1.setText("#Bedrooms");
+        toolBar1.add(label1);
+        noBed = new JSpinner();
+        noBed.setAutoscrolls(false);
+        noBed.setEnabled(true);
         toolBar1.add(noBed);
-        final JLabel label3 = new JLabel();
-        label3.setText("# Bathooms: ");
-        toolBar1.add(label3);
+        final JLabel label2 = new JLabel();
+        label2.setText("#Bathrooms");
+        toolBar1.add(label2);
+        noBath = new JSpinner();
+        noBath.setVerifyInputWhenFocusTarget(true);
         toolBar1.add(noBath);
         isFurnished = new JComboBox();
         final DefaultComboBoxModel defaultComboBoxModel2 = new DefaultComboBoxModel();
@@ -204,9 +170,6 @@ public class RenterGUI extends UserGUI {
         defaultComboBoxModel2.addElement("Unfurnished");
         isFurnished.setModel(defaultComboBoxModel2);
         toolBar1.add(isFurnished);
-        final JLabel label4 = new JLabel();
-        label4.setText("Location: ");
-        toolBar1.add(label4);
         cityQuadrant = new JComboBox();
         final DefaultComboBoxModel defaultComboBoxModel3 = new DefaultComboBoxModel();
         defaultComboBoxModel3.addElement("NE");
@@ -215,9 +178,6 @@ public class RenterGUI extends UserGUI {
         defaultComboBoxModel3.addElement("SW");
         cityQuadrant.setModel(defaultComboBoxModel3);
         toolBar1.add(cityQuadrant);
-        final JLabel label5 = new JLabel();
-        label5.setText("Price Range: ");
-        toolBar1.add(label5);
         priceRange = new JComboBox();
         final DefaultComboBoxModel defaultComboBoxModel4 = new DefaultComboBoxModel();
         defaultComboBoxModel4.addElement("$0");
@@ -238,30 +198,18 @@ public class RenterGUI extends UserGUI {
         showAllButton = new JButton();
         showAllButton.setText("Show All");
         toolBar2.add(showAllButton);
-        notificationsButton = new JButton();
-        notificationsButton.setText("Notifications");
-        toolBar2.add(notificationsButton);
-        logoutButton = new JButton();
-        logoutButton.setText("Logout");
-        toolBar2.add(logoutButton);
-        label2.setLabelFor(noBed);
-        label3.setLabelFor(noBath);
->>>>>>> parent of c208727... .
+        loginButton = new JButton();
+        loginButton.setText("Login");
+        toolBar2.add(loginButton);
+        label1.setLabelFor(noBed);
+        label2.setLabelFor(noBath);
     }
 
     /**
      * @noinspection ALL
      */
-<<<<<<< HEAD
-    public javax.swing.JComponent $$$getRootComponent$$$() {
-=======
     public JComponent $$$getRootComponent$$$() {
->>>>>>> parent of c208727... .
         return panel;
     }
 
-    private void createUIComponents() {
-        noBed = new JSpinner(new SpinnerNumberModel(0, 0, null, 1));
-        noBath = new JSpinner(new SpinnerNumberModel(0, 0, null, 1));
-    }
 }
