c30e54ab8d137aabc9df138de3b46b4cd2728e11
Melissa Picazo <1198.mp@gmail.com>
2019-11-25 10:46:38 -0700

    added fk email to db

--- TermProject/rpmdatabase.sql
+++ TermProject/rpmdatabase.sql
@@ -35,7 +35,10 @@ CREATE TABLE `properties` (
   `listingState` varchar(45) DEFAULT NULL,
   `rent` double DEFAULT NULL,
   `datePosted` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`propertyID`)
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`propertyID`),
+  KEY `email_idx` (`email`),
+  CONSTRAINT `email` FOREIGN KEY (`email`) REFERENCES `users` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -45,7 +48,7 @@ CREATE TABLE `properties` (
 
 LOCK TABLES `properties` WRITE;
 /*!40000 ALTER TABLE `properties` DISABLE KEYS */;
-INSERT INTO `properties` VALUES ('100','apartment',1,'Apple Street','A1B 2C3',1,1,2,'NE','active',750,'11/30/19'),('200','townhouse',2,'Orange Avenue','D4E 5F6',3,2,1,'SE','active',2500,'12/01/19'),('300','detached',3,'Banana Place','G7H 8I9',4,2,1,'NW','rented',5000,'10/01/19');
+INSERT INTO `properties` VALUES ('100','apartment',1,'Apple Street','A1B 2C3',1,1,2,'NE','active',750,'11/30/19','jeremy@email.com'),('200','townhouse',2,'Orange Avenue','D4E 5F6',3,2,1,'SE','active',2500,'12/01/19','jeremy@email.com'),('300','detached',3,'Banana Place','G7H 8I9',4,2,1,'NW','rented',5000,'10/01/19','jeremy@email.com');
 /*!40000 ALTER TABLE `properties` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,4 +92,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-25 10:37:06
+-- Dump completed on 2019-11-25 10:46:13
