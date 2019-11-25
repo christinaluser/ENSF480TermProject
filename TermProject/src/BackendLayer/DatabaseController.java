4bca91e17114a7626df05e775b8b79f28c05e494
Melissa Picazo <1198.mp@gmail.com>
2019-11-25 15:42:36 -0700

    updated db

--- TermProject/src/BackendLayer/DatabaseController.java
+++ TermProject/src/BackendLayer/DatabaseController.java
@@ -61,7 +61,7 @@ public class DatabaseController {   // Save as "JdbcSelectTest.java"
 
     public User validateLogin(String username, String password) {
         for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).username.equals(username) && users.get(i).password.equals(password))
+            if (users.get(i).getUsername().equals(username) && users.get(i).getPassword().equals(password))
                 return users.get(i);
         }
         return null;
